a76ae8d68fbd13ec58f62ed8a09ae37a854ffa4b
Niket Kedia <87593094+kedianiket@users.noreply.github.com>
2021-12-26 12:58:31 +0530

    Update non-numeric pivot tables with Python Pandas.py

--- non-numeric pivot tables with Python Pandas.py
+++ non-numeric pivot tables with Python Pandas.py
@@ -1,61 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
-import numpy as np
-from scipy import stats
-from datetime import datetime
 
-import matplotlib.pyplot as plt
+feedbck = pd.read_csv('feedback.csv')
 
-import seaborn as sns
-
-
-# In[2]:
-
-
-#changing working directory
-get_ipython().run_line_magic('cd', '"E:\\Python\\Pivot Table"')
-
-
-# In[3]:
-
-
-empData = pd.read_csv('mdl_feedback_dated.csv')
-
-
-# In[4]:
-
-
-empData['Key1'] = empData['course']+"-"+empData['user_id'].astype(str)
-
-
-# In[5]:
-
-
-empData['Key2'] = empData['feedback']+"-"+empData['question']
-
-
-# In[14]:
-
-
-pivot_table = empData.pivot_table(index='Key1',
-                                     columns='Key2', 
+pivot_table = feedbck.pivot_table(index='Id',
+                                     columns='Que', 
                                      values='answer_text',
-                                     aggfunc=lambda x: '###'.join(str(v) for v in x))
+                                     aggfunc=lambda x: ','.join(str(v) for v in x))
+
+pivot_table
 
 
-# In[16]:
-
-
-pivot_table.to_csv('outputtttt.csv')
-
-
-# In[ ]:
-
-
-
-
+pivot_table.to_csv('output.csv')
